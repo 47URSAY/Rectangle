db2043184a811d469d5d33a52b97c92d4798d86a
Diop <47ursay@gmail.com>
2022-10-18 12:29:23 +0200

    fait

---
+++ src/Rectangle.java
@@ -0,0 +1,33 @@
+public class Rectangle {
+    private double hauteur;
+    private double largeur;
+
+    public Rectangle(double hauteur, double largeur) {
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+    }
+
+    public double getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(double hauteur) {
+        this.hauteur = hauteur;
+    }
+
+    public double getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(double largeur) {
+        this.largeur = largeur;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "hauteur=" + hauteur +
+                ", largeur=" + largeur +
+                '}';
+    }
+}
